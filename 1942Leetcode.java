6f2651262b2b1b917c8efb62fb43af3f3470495b
Dhanu0407 <ghadagedhanashri95@gmail.com>
2024-10-11 22:53:13 +0530

    beats-33.33%

---
+++ 1942Leetcode.java
@@ -0,0 +1,52 @@
+
+public class Solution{
+    public int smallestChair(int[][] times, int targetFriend) {
+        int n = times.length;
+        int[][] friends = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            friends[i][0] = times[i][0];  
+            friends[i][1] = times[i][1];  
+            friends[i][2] = i;           
+        }
+        
+        
+        Arrays.sort(friends, Comparator.comparingInt(a -> a[0]));
+        
+        
+        PriorityQueue<Integer> availableChairs = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            availableChairs.offer(i);
+        }
+        
+        
+        PriorityQueue<int[]> occupiedChairs = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        
+        
+        for (int[] friend : friends) {
+            int arrival = friend[0];
+            int leaving = friend[1];
+            int friendIndex = friend[2];
+            
+            
+            while (!occupiedChairs.isEmpty() && occupiedChairs.peek()[0] <= arrival) {
+                int chair = occupiedChairs.poll()[1];
+                availableChairs.offer(chair);  
+            }
+            
+            
+            int assignedChair = availableChairs.poll();
+            
+            
+            if (friendIndex == targetFriend) {
+                return assignedChair;
+            }
+            
+           
+            occupiedChairs.offer(new int[] {leaving, assignedChair});
+        }
+        
+        return -1;  
+    }
+
+    
+}
