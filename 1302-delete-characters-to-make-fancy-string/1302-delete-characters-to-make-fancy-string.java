27a9e57aaa4febef3c15beb337420c3705f57529
Dhanashri Arjun Ghadage <142677482+Dhanu0407@users.noreply.github.com>
2025-07-21 23:58:52 +0530

    Time: 45 ms (25.51%), Space: 45.9 MB (40.66%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -1,16 +1,27 @@
+// class Solution {
+//     public String makeFancyString(String s) {
+//         StringBuilder answer = new StringBuilder();
+//         for(int i=0; i<s.length(); i++){
+//             char currChar = s.charAt(i);
+//             int len = answer.length();
+
+//             if(len>=2&&s.charAt(i-1)==currChar&&s.charAt(i-2)==currChar){
+//                 continue;
+//             }
+//             answer.append(currChar);
+//         }
+
+//         return answer.toString();
+//     }
+// }
+
 class Solution {
-    public String makeFancyString(String s) {
-        StringBuilder answer = new StringBuilder();
-        for(int i=0; i<s.length(); i++){
-            char currChar = s.charAt(i);
-            int len = answer.length();
-
-            if(len>=2&&s.charAt(i-1)==currChar&&s.charAt(i-2)==currChar){
-                continue;
-            }
-            answer.append(currChar);
-        }
-
-        return answer.toString();
-    }
+  public String makeFancyString(String s) {
+    StringBuilder sb = new StringBuilder();
+    for (char c : s.toCharArray())
+      if (sb.length() < 2 || //
+          sb.charAt(sb.length() - 1) != c || sb.charAt(sb.length() - 2) != c)
+        sb.append(c);
+    return sb.toString();
+  }
 }
